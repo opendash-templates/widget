2630fb85ee029a61ff7360ccb3d163cba434d101
Sebastian Zilles <dev@zilles.io>
2017-05-30 10:56:03 +0200

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# open.DASH 'widget' Template
+
+Boilerplate for an open.DASH Widget.
+
+```
+$ opendash init widget example-widget
+
+
+$ cd ./example-widget
+
+$ cd ../path/to/instance
+$ npm link ../path/to/example-widget
+
+```
---
+++ template/src/presets.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        name: 'Preset Name',
+        image: '/assets/vendor/opendash-widget-{{ opendash-widget-name }}/assets/preset.png',
+        description: 'Preset Description...',
+        config: {
+            name: 'Display Name',
+            config: {
+
+            },
+        },
+    },
+];
